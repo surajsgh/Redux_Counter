087e77398922ce1e5db358e50ad48fd35b900ba2
Suraj Singh <surajsingh220698@outlook.com>
2021-06-07 16:22:20 +0530

    Attach payloads of increment by 5 operation.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,10 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -23,6 +27,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,9 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return { counter: state.counter + 1 };
   }
+  if (action.type === "increase") {
+    return { counter: state.counter + action.amount };
+  }
   if (action.type === "decrement") {
     return { counter: state.counter - 1 };
   }
